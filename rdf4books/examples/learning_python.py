907ba8449bb8dafcc3347fa331405f09c98467c6
Gavin Carothers <gavin@carothers.name>
2011-01-02 10:12:51 -0800

    Update PURLs

--- rdf4books/examples/learning_python.py
+++ rdf4books/examples/learning_python.py
@@ -5,12 +5,12 @@ from rdflib import ConjunctiveGraph, Namespace
 complete_work_graph = ConjunctiveGraph()
 
 learning_python_work = Work.new(complete_work_graph, 
-                                "http://purl.org/NET/rdf4books/work/learning_python")
+                                "http://purl.org/net/norcalrdf/rdf4books/examples/work/learning_python")
 learning_python_work["dc:title"] = set((en("Learning Python"), 
                                        de("Python Lernen")))
 
 expressions = []
-learning_python_1e = Expression.new(complete_work_graph, "http://purl.org/NET/rdf4books/editions/learning_python_1en")
+learning_python_1e = Expression.new(complete_work_graph, "editions/learning_python_1en")
 learning_python_1e["frbr:realizationOf"] = learning_python_work
 learning_python_1e["dc:title"] = "Learning Python"
 expressions.append(learning_python_1e)
@@ -19,15 +19,15 @@ class ManifestBook(Manifestation, Book):
     """frbr:Manifestation that is also a bibo:Book"""
     pass
 
-lp_1e_print_book = ManifestBook.new(complete_work_graph, "http://purl.org/NET/rdf4books/product/isbn-9781565924642")
+lp_1e_print_book = ManifestBook.new(complete_work_graph, "http://purl.org/net/norcalrdf/rdf4books/examples/product/isbn-9781565924642")
 lp_1e_print_book["frbr:embodimentOf"] = learning_python_1e
 learning_python_1e["frbr:embodiment"] = lp_1e_print_book
 
 learning_python_work["frbr:realization"] = set(expressions)
 
-complete_work_graph.bind("work", Namespace("http://purl.org/NET/rdf4books/work/"))
-complete_work_graph.bind("product", Namespace("http://purl.org/NET/rdf4books/product/"))
-complete_work_graph.bind("editions", Namespace("http://purl.org/NET/rdf4books/editions/"))
+complete_work_graph.bind("work", Namespace("http://purl.org/net/norcalrdf/rdf4books/examples/work/"))
+complete_work_graph.bind("product", Namespace("http://purl.org/net/norcalrdf/rdf4books/examples/product/"))
+complete_work_graph.bind("editions", Namespace("http://purl.org/net/norcalrdf/rdf4books/examples/editions/"))
 
 
 print  complete_work_graph.serialize(format="n3")
